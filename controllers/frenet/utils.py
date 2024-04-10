71b9195058d2a7eaaa8f202f797722968a7b51ff
thatblueboy <f20212485@goa.bits-pilani.ac.in>
2024-04-10 10:27:35 +0530

    frenet

---
+++ controllers/frenet/utils.py
@@ -0,0 +1,614 @@
+import numpy as np
+import math
+import copy
+import bisect
+
+class CubicSpline1D:
+    """
+    1D Cubic Spline class
+
+    Parameters
+    ----------
+    x : list
+        x coordinates for data points. This x coordinates must be
+        sorted
+        in ascending order.
+    y : list
+        y coordinates for data points
+
+    Examples
+    --------
+    You can interpolate 1D data points.
+
+    >>> import numpy as np
+    >>> import matplotlib.pyplot as plt
+    >>> x = np.arange(5)
+    >>> y = [1.7, -6, 5, 6.5, 0.0]
+    >>> sp = CubicSpline1D(x, y)
+    >>> xi = np.linspace(0.0, 5.0)
+    >>> yi = [sp.calc_position(x) for x in xi]
+    >>> plt.plot(x, y, "xb", label="Data points")
+    >>> plt.plot(xi, yi , "r", label="Cubic spline interpolation")
+    >>> plt.grid(True)
+    >>> plt.legend()
+    >>> plt.show()
+
+    .. image:: cubic_spline_1d.png
+
+    """
+
+    def __init__(self, x, y):
+
+        h = np.diff(x)
+        if np.any(h < 0):
+            raise ValueError("x coordinates must be sorted in ascending order")
+
+        self.a, self.b, self.c, self.d = [], [], [], []
+        self.x = x
+        self.y = y
+        self.nx = len(x)  # dimension of x
+
+        # calc coefficient a
+        self.a = [iy for iy in y]
+
+        # calc coefficient c
+        A = self.__calc_A(h)
+        B = self.__calc_B(h, self.a)
+        self.c = np.linalg.solve(A, B)
+
+        # calc spline coefficient b and d
+        for i in range(self.nx - 1):
+            d = (self.c[i + 1] - self.c[i]) / (3.0 * h[i])
+            b = 1.0 / h[i] * (self.a[i + 1] - self.a[i]) \
+                - h[i] / 3.0 * (2.0 * self.c[i] + self.c[i + 1])
+            self.d.append(d)
+            self.b.append(b)
+
+    def calc_position(self, x):
+        """
+        Calc `y` position for given `x`.
+
+        if `x` is outside the data point's `x` range, return None.
+
+        Returns
+        -------
+        y : float
+            y position for given x.
+        """
+        if x < self.x[0]:
+            return None
+        elif x > self.x[-1]:
+            return None
+
+        i = self.__search_index(x)
+        dx = x - self.x[i]
+        position = self.a[i] + self.b[i] * dx + \
+            self.c[i] * dx ** 2.0 + self.d[i] * dx ** 3.0
+
+        return position
+
+    def calc_first_derivative(self, x):
+        """
+        Calc first derivative at given x.
+
+        if x is outside the input x, return None
+
+        Returns
+        -------
+        dy : float
+            first derivative for given x.
+        """
+
+        if x < self.x[0]:
+            return None
+        elif x > self.x[-1]:
+            return None
+
+        i = self.__search_index(x)
+        dx = x - self.x[i]
+        dy = self.b[i] + 2.0 * self.c[i] * dx + 3.0 * self.d[i] * dx ** 2.0
+        return dy
+
+    def calc_second_derivative(self, x):
+        """
+        Calc second derivative at given x.
+
+        if x is outside the input x, return None
+
+        Returns
+        -------
+        ddy : float
+            second derivative for given x.
+        """
+
+        if x < self.x[0]:
+            return None
+        elif x > self.x[-1]:
+            return None
+
+        i = self.__search_index(x)
+        dx = x - self.x[i]
+        ddy = 2.0 * self.c[i] + 6.0 * self.d[i] * dx
+        return ddy
+
+    def __search_index(self, x):
+        """
+        search data segment index
+        """
+        return bisect.bisect(self.x, x) - 1
+
+    def __calc_A(self, h):
+        """
+        calc matrix A for spline coefficient c
+        """
+        A = np.zeros((self.nx, self.nx))
+        A[0, 0] = 1.0
+        for i in range(self.nx - 1):
+            if i != (self.nx - 2):
+                A[i + 1, i + 1] = 2.0 * (h[i] + h[i + 1])
+            A[i + 1, i] = h[i]
+            A[i, i + 1] = h[i]
+
+        A[0, 1] = 0.0
+        A[self.nx - 1, self.nx - 2] = 0.0
+        A[self.nx - 1, self.nx - 1] = 1.0
+        return A
+
+    def __calc_B(self, h, a):
+        """
+        calc matrix B for spline coefficient c
+        """
+        B = np.zeros(self.nx)
+        for i in range(self.nx - 2):
+            B[i + 1] = 3.0 * (a[i + 2] - a[i + 1]) / h[i + 1]\
+                - 3.0 * (a[i + 1] - a[i]) / h[i]
+        return B
+
+class CubicSpline2D:
+    """
+    Cubic CubicSpline2D class
+
+    Parameters
+    ----------
+    x : list
+        x coordinates for data points.
+    y : list
+        y coordinates for data points.
+
+    Examples
+    --------
+    You can interpolate a 2D data points.
+
+    >>> import matplotlib.pyplot as plt
+    >>> x = [-2.5, 0.0, 2.5, 5.0, 7.5, 3.0, -1.0]
+    >>> y = [0.7, -6, 5, 6.5, 0.0, 5.0, -2.0]
+    >>> ds = 0.1  # [m] distance of each interpolated points
+    >>> sp = CubicSpline2D(x, y)
+    >>> s = np.arange(0, sp.s[-1], ds)
+    >>> rx, ry, ryaw, rk = [], [], [], []
+    >>> for i_s in s:
+    ...     ix, iy = sp.calc_position(i_s)
+    ...     rx.append(ix)
+    ...     ry.append(iy)
+    ...     ryaw.append(sp.calc_yaw(i_s))
+    ...     rk.append(sp.calc_curvature(i_s))
+    >>> plt.subplots(1)
+    >>> plt.plot(x, y, "xb", label="Data points")
+    >>> plt.plot(rx, ry, "-r", label="Cubic spline path")
+    >>> plt.grid(True)
+    >>> plt.axis("equal")
+    >>> plt.xlabel("x[m]")
+    >>> plt.ylabel("y[m]")
+    >>> plt.legend()
+    >>> plt.show()
+
+    .. image:: cubic_spline_2d_path.png
+
+    >>> plt.subplots(1)
+    >>> plt.plot(s, [np.rad2deg(iyaw) for iyaw in ryaw], "-r", label="yaw")
+    >>> plt.grid(True)
+    >>> plt.legend()
+    >>> plt.xlabel("line length[m]")
+    >>> plt.ylabel("yaw angle[deg]")
+
+    .. image:: cubic_spline_2d_yaw.png
+
+    >>> plt.subplots(1)
+    >>> plt.plot(s, rk, "-r", label="curvature")
+    >>> plt.grid(True)
+    >>> plt.legend()
+    >>> plt.xlabel("line length[m]")
+    >>> plt.ylabel("curvature [1/m]")
+
+    .. image:: cubic_spline_2d_curvature.png
+    """
+
+    def __init__(self, x, y):
+        self.s = self.__calc_s(x, y)
+        self.sx = CubicSpline1D(self.s, x)
+        self.sy = CubicSpline1D(self.s, y)
+
+    def __calc_s(self, x, y):
+        dx = np.diff(x)
+        dy = np.diff(y)
+        self.ds = np.hypot(dx, dy)
+        s = [0]
+        s.extend(np.cumsum(self.ds))
+        return s
+
+    def calc_position(self, s):
+        """
+        calc position
+
+        Parameters
+        ----------
+        s : float
+            distance from the start point. if `s` is outside the data point's
+            range, return None.
+
+        Returns
+        -------
+        x : float
+            x position for given s.
+        y : float
+            y position for given s.
+        """
+        x = self.sx.calc_position(s)
+        y = self.sy.calc_position(s)
+
+        return x, y
+
+    def calc_curvature(self, s):
+        """
+        calc curvature
+
+        Parameters
+        ----------
+        s : float
+            distance from the start point. if `s` is outside the data point's
+            range, return None.
+
+        Returns
+        -------
+        k : float
+            curvature for given s.
+        """
+        dx = self.sx.calc_first_derivative(s)
+        ddx = self.sx.calc_second_derivative(s)
+        dy = self.sy.calc_first_derivative(s)
+        ddy = self.sy.calc_second_derivative(s)
+        k = (ddy * dx - ddx * dy) / ((dx ** 2 + dy ** 2)**(3 / 2))
+        return k
+
+    def calc_yaw(self, s):
+        """
+        calc yaw
+
+        Parameters
+        ----------
+        s : float
+            distance from the start point. if `s` is outside the data point's
+            range, return None.
+
+        Returns
+        -------
+        yaw : float
+            yaw angle (tangent vector) for given s.
+        """
+        dx = self.sx.calc_first_derivative(s)
+        dy = self.sy.calc_first_derivative(s)
+        yaw = math.atan2(dy, dx)
+        return yaw
+
+class QuinticPolynomial:
+    """
+    A class representing a quintic polynomial trajectory.
+
+    Args:
+            xs (float): Initial position.
+            vxs (float): Initial velocity.
+            axs (float): Initial acceleration.
+            xe (float): Final position.
+            vxe (float): Final velocity.
+            axe (float): Final acceleration.
+            time (float): Total time duration.
+    """
+    def __init__(self, xs, vxs, axs, xe, vxe, axe, time):
+        # calc coefficient of quintic polynomial
+        # See jupyter notebook document for derivation of this equation.
+        self.a0 = xs
+        self.a1 = vxs
+        self.a2 = axs / 2.0
+
+        A = np.array([[time ** 3, time ** 4, time ** 5],
+                      [3 * time ** 2, 4 * time ** 3, 5 * time ** 4],
+                      [6 * time, 12 * time ** 2, 20 * time ** 3]])
+        b = np.array([xe - self.a0 - self.a1 * time - self.a2 * time ** 2,
+                      vxe - self.a1 - 2 * self.a2 * time,
+                      axe - 2 * self.a2])
+        x = np.linalg.solve(A, b)
+
+        self.a3 = x[0]
+        self.a4 = x[1]
+        self.a5 = x[2]
+
+    def calc_point(self, t):
+        """
+        returns f(t) 0<t<time
+        """
+        xt = self.a0 + self.a1 * t + self.a2 * t ** 2 + \
+             self.a3 * t ** 3 + self.a4 * t ** 4 + self.a5 * t ** 5
+
+        return xt
+
+    def calc_first_derivative(self, t):
+        """
+        returns f'(t), 0<t<time
+        """
+        xt = self.a1 + 2 * self.a2 * t + \
+             3 * self.a3 * t ** 2 + 4 * self.a4 * t ** 3 + 5 * self.a5 * t ** 4
+
+        return xt
+
+    def calc_second_derivative(self, t):
+        """
+        returns f''(t), 0<t<time
+        """
+        xt = 2 * self.a2 + 6 * self.a3 * t + 12 * self.a4 * t ** 2 + 20 * self.a5 * t ** 3
+
+        return xt
+
+    def calc_third_derivative(self, t):
+        """
+        returns f'''(t), 0<t<time
+        """
+        xt = 6 * self.a3 + 24 * self.a4 * t + 60 * self.a5 * t ** 2
+
+        return xt
+
+class QuarticPolynomial:
+
+    def __init__(self, xs, vxs, axs, vxe, axe, time):
+        # calc coefficient of quartic polynomial
+
+        self.a0 = xs
+        self.a1 = vxs
+        self.a2 = axs / 2.0
+
+        A = np.array([[3 * time ** 2, 4 * time ** 3],
+                      [6 * time, 12 * time ** 2]])
+        b = np.array([vxe - self.a1 - 2 * self.a2 * time,
+                      axe - 2 * self.a2])
+        x = np.linalg.solve(A, b)
+
+        self.a3 = x[0]
+        self.a4 = x[1]
+
+    def calc_point(self, t):
+        xt = self.a0 + self.a1 * t + self.a2 * t ** 2 + \
+        self.a3 * t ** 3 + self.a4 * t ** 4
+
+        return xt
+
+    def calc_first_derivative(self, t):
+        xt = self.a1 + 2 * self.a2 * t + \
+             3 * self.a3 * t ** 2 + 4 * self.a4 * t ** 3
+
+        return xt
+
+    def calc_second_derivative(self, t):
+        xt = 2 * self.a2 + 6 * self.a3 * t + 12 * self.a4 * t ** 2
+
+        return xt
+
+    def calc_third_derivative(self, t):
+        xt = 6 * self.a3 + 24 * self.a4 * t
+
+        return xt
+
+class FrenetPath:
+
+    def __init__(self):
+        self.t = []
+        self.d = []
+        self.d_d = []
+        self.d_dd = []
+        self.d_ddd = []
+        self.s = []
+        self.s_d = []
+        self.s_dd = []
+        self.s_ddd = []
+        self.cd = 0.0
+        self.cv = 0.0
+        self.cf = 0.0
+
+        self.x = []
+        self.y = []
+        self.yaw = []
+        self.ds = []
+        self.c = []
+
+class FrenetPlanner():
+    def __init__(self, goal_list):
+        self.MAX_SPEED = 50.0 / 3.6  # maximum speed [m/s]
+        self.MAX_ACCEL = 2.0  # maximum acceleration [m/ss]
+        self.MAX_CURVATURE = 1.0  # maximum curvature [1/m]
+        self.MAX_ROAD_WIDTH = 7.0  # maximum road width [m]
+        self.D_ROAD_W = 1.0  # road width sampling length [m]
+        self.DT = 0.2  # time tick [s]
+        self.MAX_T = 5.0  # max prediction time [s]
+        self.MIN_T = 4.0  # min prediction time [s]
+        self.TARGET_SPEED = 30.0 / 3.6  # target speed [m/s]
+        self.D_T_S = 5.0 / 3.6  # target speed sampling length [m/s]
+        self.N_S_SAMPLE = 1  # sampling number of target speed
+        self.ROBOT_RADIUS = 2.0  # robot radius [m]
+       
+        self.K_J = 0.1
+        self.K_T = 0.1
+        self.K_D = 1.0
+        self.K_LAT = 1.0
+        self.K_LON = 1.0
+
+        wx = []
+        wy = []
+        self.nearest_s_index = 0
+        for goal in goal_list:
+            wx.append(goal[0])
+            wy.append(goal[1])
+        print(wx)
+        print(wy)
+        self.csp = CubicSpline2D(wx, wy)
+
+    def frenet_optimal_planning(self, s0, c_speed, c_accel, c_d, c_d_d, c_d_dd):
+        fplist = self.calc_frenet_paths(c_speed, c_accel, c_d, c_d_d, c_d_dd, s0)
+        fplist = self.calc_global_paths(fplist)
+        fplist = self.check_paths(fplist)
+
+        # find minimum cost path
+        min_cost = float("inf")
+        best_path = None
+        for fp in fplist:
+            if min_cost >= fp.cf:
+                min_cost = fp.cf
+                best_path = fp
+
+    def calc_global_paths(self,fplist):
+        for fp in fplist:
+            # calc global positions
+            for i in range(len(fp.s)):
+                ix, iy = self.csp.calc_position(fp.s[i])
+                if ix is None:
+                    break
+                i_yaw = self.csp.calc_yaw(fp.s[i])
+                di = fp.d[i]
+                fx = ix + di * math.cos(i_yaw + math.pi / 2.0)
+                fy = iy + di * math.sin(i_yaw + math.pi / 2.0)
+                fp.x.append(fx)
+                fp.y.append(fy)
+
+            # calc yaw and ds
+            for i in range(len(fp.x) - 1):
+                dx = fp.x[i + 1] - fp.x[i]
+                dy = fp.y[i + 1] - fp.y[i]
+                fp.yaw.append(math.atan2(dy, dx))
+                fp.ds.append(math.hypot(dx, dy))
+
+            fp.yaw.append(fp.yaw[-1])
+            fp.ds.append(fp.ds[-1])
+
+            # calc curvature
+            for i in range(len(fp.yaw) - 1):
+                fp.c.append((fp.yaw[i + 1] - fp.yaw[i]) / fp.ds[i])
+
+            return fplist
+
+    def calc_frenet_paths(self, c_speed, c_accel, c_d, c_d_d, c_d_dd, s0):
+        frenet_paths = []
+
+        # generate path to each offset goal
+        for di in np.arange(-self.MAX_ROAD_WIDTH, self.MAX_ROAD_WIDTH, self.D_ROAD_W):
+
+            # Lateral motion planning
+            for Ti in np.arange(self.MIN_T, self.MAX_T, self.DT):
+                fp = FrenetPath()
+
+                lat_qp = QuinticPolynomial(c_d, c_d_d, c_d_dd, di, 0.0, 0.0, Ti)
+
+                fp.t = [t for t in np.arange(0.0, Ti, self.DT)]
+                fp.d = [lat_qp.calc_point(t) for t in fp.t]
+                fp.d_d = [lat_qp.calc_first_derivative(t) for t in fp.t]
+                fp.d_dd = [lat_qp.calc_second_derivative(t) for t in fp.t]
+                fp.d_ddd = [lat_qp.calc_third_derivative(t) for t in fp.t]
+
+                # Longitudinal motion planning (Velocity keeping)
+                for tv in np.arange(self.TARGET_SPEED - self.D_T_S * self.N_S_SAMPLE,
+                                    self.TARGET_SPEED + self.D_T_S * self.N_S_SAMPLE, self.D_T_S):
+                    tfp = copy.deepcopy(fp)
+                    lon_qp = QuarticPolynomial(s0, c_speed, c_accel, tv, 0.0, Ti)
+
+                    tfp.s = [lon_qp.calc_point(t) for t in fp.t]
+                    tfp.s_d = [lon_qp.calc_first_derivative(t) for t in fp.t]
+                    tfp.s_dd = [lon_qp.calc_second_derivative(t) for t in fp.t]
+                    tfp.s_ddd = [lon_qp.calc_third_derivative(t) for t in fp.t]
+
+                    Jp = sum(np.power(tfp.d_ddd, 2))  # square of jerk
+                    Js = sum(np.power(tfp.s_ddd, 2))  # square of jerk
+
+                    # square of diff from target speed
+                    ds = (self.TARGET_SPEED - tfp.s_d[-1]) ** 2
+
+                    tfp.cd = self.K_J * Jp + self.K_T * Ti + self.K_D * tfp.d[-1] ** 2
+                    tfp.cv = self.K_J * Js + self.K_T * Ti + self.K_D * ds
+                    tfp.cf = self.K_LAT * tfp.cd + self.K_LON * tfp.cv
+
+                    frenet_paths.append(tfp)
+
+        return frenet_paths
+    
+    def check_paths(self, fplist):
+        ok_ind = []
+        for i, _ in enumerate(fplist):
+            if any([v > self.MAX_SPEED for v in fplist[i].s_d]):  # Max speed check
+                continue
+            elif any([abs(a) > self.MAX_ACCEL for a in
+                      fplist[i].s_dd]):  # Max accel check
+                continue
+            elif any([abs(c) > self.MAX_CURVATURE for c in
+                      fplist[i].c]):  # Max curvature check
+                continue
+            elif not self.check_collision(fplist[i], self.obs_list):
+                continue
+
+            ok_ind.append(i)
+
+        return [fplist[i] for i in ok_ind]
+
+    def check_collision(self, fp, ob):
+        for i in range(len(ob[:, 0])):
+            d = [((ix - ob[i, 0]) ** 2 + (iy - ob[i, 1]) ** 2)
+                 for (ix, iy) in zip(fp.x, fp.y)]
+
+            collision = any([di <= self.ROBOT_RADIUS ** 2 for di in d])
+
+            if collision:
+                return False
+
+        return True
+    
+    def get_path(self, pos, vel, acc, obs_list = []):
+        self.obs_list = obs_list
+        
+        # find s in csp that is closest to pos
+        closest_s = None
+        min_distance = np.inf
+        print(len(self.csp.s))
+        for s in self.csp.s:
+            x, y = self.csp.calc_position(s)
+            distance = ((pos[0] - x)**2 + (pos[1] - y)**2)**0.5
+            if distance < min_distance:
+                min_distance = distance
+                closest_s = s
+
+        d = (pos - self.csp.calc_position(closest_s)) #vector along d in frenet frame
+        d_unit = d/np.linalg.norm(d) 
+
+        c_d = d
+        c_d_d = np.dot(vel, d_unit)
+        c_d_dd = np.dot(acc, d_unit)
+        s0 = closest_s
+        c_speed = vel - c_d_d
+        c_accel = acc - c_d_dd
+
+        rx, ry, ryaw, rk = [], [], [], []
+        for i_s in self.csp.s:
+            ix, iy = self.csp.calc_position(i_s)
+            rx.append(ix)
+            ry.append(iy)
+            ryaw.append(self.csp.calc_yaw(i_s))
+            rk.append(self.csp.calc_curvature(i_s))
+
+        path = self.frenet_optimal_planning(self, s0, c_speed, c_accel, c_d, c_d_d, c_d_dd)
+        
+
+
+    
